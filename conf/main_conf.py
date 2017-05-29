50633b2d1653b4af836c9b1394be2e54561c528f
spgreen <simonpetergreen@singaren.net.sg>
2017-05-29 15:29:24 +0800

    Update main_conf.py

--- conf/main_conf.py
+++ conf/main_conf.py
@@ -1,10 +1,10 @@
 file_path_failed_tests = "failed.json"
 file_path_status = "status.json"
-file_path_network_peers = "singpeers_geo.json"
+file_path_network_peers = "routers_geo.json"
 
 ping_count = 1
 ping_timeout = 5
 
-mail_to_address = 'Simon Peter Green <simonpetergreen@singaren.net.sg>'
-mail_from_address = 'Status Update <no-reply@singaren.net.sg>'
-mail_subject = 'SingAREN Peer Router Status' 
+mail_to_address = 'To Address <to-address@example.com>'
+mail_from_address = 'Status Update <no-reply@example.com>'
+mail_subject = 'Peer Router Status' 
