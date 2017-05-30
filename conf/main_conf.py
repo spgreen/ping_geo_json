7c90f5d1e6073354ada8ed02059b50d7ba545f29
spgreen <simonpetergreen@singaren.net.sg>
2017-05-30 15:45:34 +0800

    Removed ping_timeout; added ping_warn_percent_loss
    ping_warn_percent_loss variable used for check_ping2 function as a status threshold for ping loss percentage.

--- conf/main_conf.py
+++ conf/main_conf.py
@@ -3,7 +3,7 @@ file_path_status = "status.json"
 file_path_network_peers = "routers_geo.json"
 
 ping_count = 1
-ping_timeout = 5
+ping_warn_percent_loss = 20
 
 mail_to_address = 'To Address <to-address@example.com>'
 mail_from_address = 'Status Update <no-reply@example.com>'
